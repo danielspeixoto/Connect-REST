30b151470a47491000e2e883adfd43f6d864cffa
Daniel Peixoto <danielspeixoto@outlook.com>
2017-04-13 02:41:10 -0300

    d

--- model/user.js
+++ model/user.js
@@ -4,41 +4,37 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
 	name: {
         type: String,
-        required: true
     },
     username: {
         type: String,
-        required: true
-    }
+    },
     password: {
         type: String,
-        required: true
-    }
+    },
     group: {
         type: String,
-        required: true
     }
 });
 
 const User = module.exports = mongoose.model('User', userSchema);
 
 // Get Users
-module.exports.getUsers = (callback, limit) => {
+module.exports.getUsers = function(callback, limit) {
 	User.find(callback).limit(limit);
 }
 
 // Get User
-module.exports.getUserById = (id, callback) => {
+module.exports.getUserById = function(id, callback){
 	User.findById(id, callback);
 }
 
 // Add User
-module.exports.addUser = (user, callback) => {
+module.exports.addUser = function(user, callback) {
 	User.create(user, callback);
 }
 
 // Update User
-module.exports.updateUser = (id, user, options, callback) => {
+module.exports.updateUser = function(id, user, options, callback){
 	var query = {_id: id};
 	var update = {
         name : user.name,
@@ -50,7 +46,7 @@ module.exports.updateUser = (id, user, options, callback) => {
 }
 
 // Delete User
-module.exports.removeUser = (id, callback) => {
+module.exports.removeUser = function(id, callback){
 	var query = {_id: id};
 	User.remove(query, callback);
 }
